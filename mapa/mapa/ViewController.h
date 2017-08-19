daa91e452c1a3cc9fb4fa196225959e14828f423
Treinamento <treinamento@Treinamento1-Maquina2.local>
2017-08-19 09:08:01 -0300

    exercise6 mapas

---
+++ mapa/mapa/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  mapa
+//
+//  Created by Treinamento on 19/08/17.
+//  Copyright © 2017 Treinamento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface ViewController : UIViewController<CLLocationManagerDelegate,MKMapViewDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) CLLocationManager *locationManager;
+
+@end
+
---
+++ mapa/mapa/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  mapa
+//
+//  Created by Treinamento on 19/08/17.
+//  Copyright © 2017 Treinamento. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
